53e58279fedbc2f87357e630108f8eefab09bf06
RobotNinja07 <57858475+RobotNinja07@users.noreply.github.com>
2022-02-16 13:35:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $json_data = json_decode($json_file,true);
     <div data-bs-offset="0" tabindex="0">
       <div id="about" class="content-item">
         <div class="vertical-center">
-          <h1><?php echo $json_data['first-name']; ?><span class="text-primary"><?php echo $json_data['last-name']; ?></span></h1>
+          <h1><?php echo $json_data['first-name']; ?> <span class="text-primary"><?php echo $json_data['last-name']; ?></span></h1>
           <div class="subheading mb-3">
             <?php echo $json_data['address']; ?> · <?php echo $json_data['phone']; ?> ·
             <a href="<?php echo $json_data['email']; ?>"><?php echo $json_data['email']; ?></a>
